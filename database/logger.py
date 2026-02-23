c8d1b7af464fc73a10f3e82915b39cfcd471609b
Noeh-mi <68011374@kmitl.ac.th>
2026-02-23 13:45:06 +0700

    added session separator function to logger file

--- database/logger.py
+++ database/logger.py
@@ -6,11 +6,29 @@ from datetime import datetime
 LOG_FILE = "data/transactions.log"
 
 
-def log_event(event_type: str, medication: str, remaining: int):
-    os.makedirs("data", exist_ok=True)  # <-- auto-create folder
+def log_event(event_type: str, source: str, message: str):
+    os.makedirs("data", exist_ok=True)
 
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    entry = f"{timestamp} | {event_type} | {medication} | Remaining: {remaining}\n"
+    entry = f"{timestamp} | {event_type} | {source} | {message}\n"
 
     with open(LOG_FILE, "a") as f:
         f.write(entry)
+
+from datetime import datetime
+import os
+
+LOG_FILE = "data/transactions.log"
+
+
+def log_sess_separator():
+    os.makedirs("data", exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    separator_line = (
+        f"========================== SESSION ENDED {timestamp} =========================="
+    )
+
+    with open(LOG_FILE, "a") as f:
+        f.write(separator_line)
